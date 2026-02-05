aedcfd3af5bbb33ad2418e7807d2a42b69bd1dda
Aryan Mane <aryanmane@Aryans-MacBook-Pro.local>
2026-02-05 18:15:02 +0530

    koyeb deploy

--- app.py
+++ app.py
@@ -71,6 +71,8 @@ def handle_message(data):
 
     emit("receive_message", message_data, broadcast=True)
 
+import os
 
 if __name__ == "__main__":
-    socketio.run(app, debug=True)
+    port = int(os.environ.get("PORT", 8000))
+    socketio.run(app, host="0.0.0.0", port=port)
